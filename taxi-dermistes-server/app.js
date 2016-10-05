d925da1b9ca1ef876290415d125064fff32b12e5
Goodman Christopher <11200452@T1700-debian.ig-edu.univ-paris13.fr>
2016-10-05 10:39:32 +0200

    Test envoi de fichiers

--- taxi-dermistes-server/app.js
+++ taxi-dermistes-server/app.js
@@ -9,6 +9,14 @@ app.get('/~', function(req, res){
         res.send("home")
 });
 
+app.get('/image', function(req, res){
+        res.sendFile("/export/home/users/master/master2/m2pls2017/11200452/git/JSAU/taxi-dermistes-server/files/image.png")
+});
+
+app.get('/page', function(req, res){
+        res.sendFile("/export/home/users/master/master2/m2pls2017/11200452/git/JSAU/taxi-dermistes-server/files/page.html")
+});
+
 app.post('/', function(req,res){
 	res.send("racine post")
 });
