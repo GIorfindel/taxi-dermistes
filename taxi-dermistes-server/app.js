7b669a82beb96d9f00b9cb7d7f2b2840caba2073
Glorfindel <floflo_93290@hotmail.fr>
2016-10-18 18:28:07 +0200

    verification mail

--- taxi-dermistes-server/app.js
+++ taxi-dermistes-server/app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express();
-var logger = require('tracer').colorConsole();
+let express = require('express');
+let app = express();
+let logger = require('tracer').colorConsole();
+let validator = require('validator');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('json spaces',0)
 
 
 //PORT DU SERVEUR
@@ -50,8 +52,8 @@ app.post('/', function(req,res){
 
 /************** GET **************/
 app.get('/clients/:client_id', function(req, res){
-        var listeUser = require('./clients.json');
-        var exists = false;
+        let listeUser = require('./clients.json');
+        let exists = false;
         for(index in listeUser.clients) {
                 if(listeUser.clients[index].client_id==req.params["client_id"]){
                 	res.status(200); // Envoi le code HTTP 200 : OK
@@ -67,7 +69,7 @@ app.get('/clients/:client_id', function(req, res){
 });
 
 app.get('/clients', function(req, res) {
-	var listeUser = require('./clients.json');
+	let listeUser = require('./clients.json');
 	if(listeUser.clients.length > 0) {
 		res.status(200);
 		res.send(listeUser.clients);
@@ -81,30 +83,36 @@ app.get('/clients', function(req, res) {
 
 /************** POST **************/
 app.post('/clients', function(req, res){
-	var obj = require('./clients.json');
-	req.body.client_id=obj.libre;
-	obj.clients.push(req.body);
-	obj.libre=obj.libre+1;
-	fs.writeFile('clients.json', '')
-   	fs.appendFile('clients.json', JSON.stringify(obj)+"\n", (err) => {
-  		if (err) {
-  			logger.error(err);
-  			res.status(500);
-  			res.send({"error":"Impossible de traiter la requête"});
-  		}
-  		else {
-	  		logger.trace("Client ajouté avec l'id %d", (obj.libre-1));
-	  		res.status(201); // 201 == Created
-			res.send({"status": "success"});
-	  	}
-	});
-	
+	let obj = require('./clients.json');
+  if ((req.body.client_mail==null) || (validator.isEmail(req.body.client_mail) == false))
+  {
+    res.status(400); //Bad Request
+    res.send({"error":"Votre adresse mail n'est pas valide"});
+  }
+  else {
+  	req.body.client_id=obj.libre;
+  	obj.clients.push(req.body);
+  	obj.libre=obj.libre+1;
+  	fs.writeFile('clients.json', '')
+     	fs.appendFile('clients.json', JSON.stringify(obj)+"\n", (err) => {
+    		if (err) {
+    			logger.error(err);
+    			res.status(500);
+    			res.send({"error":"Impossible de traiter la requête"});
+    		}
+    		else {
+  	  		logger.trace("Client ajouté avec l'id %d", (obj.libre-1));
+  	  		res.status(201); // 201 == Created
+  			res.send({"status": "success"});
+  	  	}
+  	});
+  }
 });
 
 /************** DELETE **************/
 app.delete('/clients', function(req, res){
-        var obj = require('./clients.json');
-        var exists = false;
+        let obj = require('./clients.json');
+        let exists = false;
         for(index in obj.clients) {
         	if(obj.clients[index].client_id==req.body["client_id"]){
         		exists = true;
@@ -135,51 +143,39 @@ app.delete('/clients', function(req, res){
 
 /************** PUT **************/
 app.put('/clients', function(req, res){
-        var obj = require('./clients.json');
-        var exists = false;
-        for(index in obj.clients) {
-        	if(obj.clients[index].client_id==req.body["client_id"]){
-        		exists = true;
-             	obj.clients[index].client_name=req.body["client_name"];
-            }
+        let obj = require('./clients.json');
+        if ((req.body.client_mail==null) || (validator.isEmail(req.body.client_mail) == false))
+        {
+          res.status(400); //Bad Request
+          res.send({"error":"Votre adresse mail n'est pas valide"});
         }
-        if(exists) {
-		    fs.writeFile('clients.json', '')
-		    fs.appendFile('clients.json', JSON.stringify(obj)+"\n", (err) => {
-		    	if (err) {
-		  			logger.error(err);
-		  			res.status(500);
-		  			res.send({"error":"Impossible de traiter la requête"});
-		  		}
-		  		else {
-		    		logger.trace("Client %d modifié", req.body["client_id"]);
-		    		res.status(200);
-					res.send({"status": "success"});
-				}
-		    });
-		}
-		else {
-			logger.warn("Impossible de modifier le client %d", req.body["client_id"]);
-		  	res.status(404);
-		  	res.send({"error":"Client inexistant"});
-		}
+        else {
+          let exists = false;
+          for(index in obj.clients) {
+          	if(obj.clients[index].client_id==req.body["client_id"]){
+          		exists = true;
+               	obj.clients[index].client_name=req.body["client_name"];
+              }
+          }
+          if(exists) {
+  		    fs.writeFile('clients.json', '')
+  		    fs.appendFile('clients.json', JSON.stringify(obj)+"\n", (err) => {
+  		    	if (err) {
+  		  			logger.error(err);
+  		  			res.status(500);
+  		  			res.send({"error":"Impossible de traiter la requête"});
+  		  		}
+  		  		else {
+  		    		logger.trace("Client %d modifié", req.body["client_id"]);
+  		    		res.status(200);
+  					res.send({"status": "success"});
+  				}
+  		    });
+  		}
+  		else {
+  			logger.warn("Impossible de modifier le client %d", req.body["client_id"]);
+  		  	res.status(404);
+  		  	res.send({"error":"Client inexistant"});
+  		}
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
