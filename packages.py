f5b4dea9b09c663026a18dd2d5cbc1d140ca999f
Glorfindel <floflo_93290@hotmail.fr>
2016-10-18 18:29:27 +0200

    ajout package validator

--- packages.py
+++ packages.py
@@ -2,3 +2,6 @@ import os
 os.chdir('taxi-dermistes-server')
 os.system('npm install tracer --save')
 os.system('npm install express --save')
+os.system('npm install body-parser --save')
+os.system('npm install validator --save')
+
