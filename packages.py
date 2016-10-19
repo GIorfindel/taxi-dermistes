7db24a04d56e9abef5e14a83d30e6018ce2d32af
GIorfindel <florian.choiselle@free.fr>
2016-10-19 11:16:00 +0200

    ajout eslint et browerify

--- packages.py
+++ packages.py
@@ -4,4 +4,7 @@ os.system('npm install tracer --save')
 os.system('npm install express --save')
 os.system('npm install body-parser --save')
 os.system('npm install validator --save')
+os.system('npm install eslint --save-dev')
+os.system('npm install eslint-config-usecases --save-dev')
+os.system('npm install -g browserify  --save')
 
