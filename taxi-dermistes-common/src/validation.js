52634c988874b2efc8dcabf18cc456aadf699d6e
Glorfindel <floflo_93290@hotmail.fr>
2017-01-04 09:56:43 +0100

    ajout common

---
+++ taxi-dermistes-common/src/validation.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let validator = require('validator')
+
+// Ici votre code : remplacer ce module par votre code à vous
+
+function isDefined(object) {
+    return (typeof object !== 'undefined' || object)
+}
+
+function isValidEmail(email) {
+    return validator.isEmail(email)
+}
+
+/**
+ * Returns wether the given date is a valid date in the future
+ */
+function isValidFutureDate(date) {
+    return validator.isDate(date.toString()) && validator.isAfter(date.toString())
+}
+
+function isValidId(id) {
+    return validator.isNumeric(id)
+}
+
+function isEmpty(str) {
+    return validator.isEmpty(str)
+}
+
+exports.isValidEmail = isValidEmail
+exports.isValidFutureDate = isValidFutureDate
+exports.isDefined = isDefined
+exports.isValidId = isValidId
+exports.isEmpty = isEmpty
